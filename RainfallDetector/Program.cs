8e79b1a382e3fd7d44cd14c47bd8a233494d91f2
Nabin Gurung <nabin.gurung.email@gmail.com>
2025-04-19 14:37:38 +0800

    Initialization and execution of the application.

--- RainfallDetector/Program.cs
+++ RainfallDetector/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RainfallDetector.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //File source
+        List<string> filePaths = new List<string>() { 
+            "Assets/Data1.csv" ,
+            "Assets/Data2.csv" ,
+            "Assets/Devices.csv"
+        };
+
+
+        //Initialization
+        IRainfallCalculator rainfallCalculator = new RainfallCalculator();
+
+        //Execution
+        rainfallCalculator.Get(filePaths);
+    }
+}
---
+++ RainfallDetectorTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RainfallDetectorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
