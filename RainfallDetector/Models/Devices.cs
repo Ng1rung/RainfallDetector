eadb05b08a5dd0718efb90f145628a14dfa6492f
Nabin Gurung <nabin.gurung.email@gmail.com>
2025-04-19 11:41:39 +0800

    Models for rainfall reading. Used for mapping of data.

---
+++ RainfallDetector/Models/Devices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallDetector.Models
+{
+    public class Devices
+    {
+        public int DeviceID { get; set; }
+        public string DeviceName { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ RainfallDetector/Models/RainfallData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallDetector.Models
+{
+    public class RainfallData
+    {
+        public int DeviceID { get; set; }
+        public DateTime DateTime { get; set; }
+        public float Measurement { get; set; }
+    }
+}
